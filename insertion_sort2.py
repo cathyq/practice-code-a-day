e4c3882df0896df05de2ca065e0f9734d2df468f
cathyq <cathyqiujing@gmail.com>
2017-03-22 21:19:49 -0600

    adding another method

--- insertion_sort2.py
+++ insertion_sort2.py
@@ -5,7 +5,7 @@
 
 s = 6
 ar = [1,4,3,5,6,2]
-
+# Method-1
 def insertionSort(ar):
     for i in range(1,s):  
         pivot = ar[i]    
@@ -18,6 +18,25 @@ def insertionSort(ar):
         
     #for k in ar: return k
     return ar
+
+# Method-2
+def insertionSort(ar):    
+    for a0 in range(1, s):                
+        pivot = ar[a0]        
+        for i in reversed(range(a0)):   
+            if ar[i] > pivot:
+                ar[i+1] = ar[i]
+                if i==0:
+                    ar[i]=pivot
+                #a[i], a[i+1] = a[i+1], a[i]
+            elif ar[i] <= pivot:
+                ar[i+1] = pivot
+                break
+        for j in ar: print j,
+        print
+       
+    return ar 
+
 
 
 insertionSort(ar)
