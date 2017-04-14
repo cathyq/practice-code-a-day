298512fef87e4cbf5f10834c5ec7727a59bd744e
cathyq <cathyqiujing@gmail.com>
2017-04-13 20:44:21 -0600

    added a new method for merge the tool problem

--- merge_the_tool.py
+++ merge_the_tool.py
@@ -1,4 +1,5 @@
 # Problem from hackerrank: https://www.hackerrank.com/challenges/merge-the-tools
+# Method 1
 def merge_the_tools(string, k): 
     sub = len(string)/k
     new = []
@@ -11,6 +12,27 @@ def merge_the_tools(string, k):
             print result
             new[:] = []
 
+
+print "Test 1"
+string = "AABCAAADA"
+k=3
+merge_the_tools(string, k)
+print
+
+# method 2
+def merge_the_tools(string, k): 
+    new = []
+    seen = {}
+    for index, i in enumerate(list(string)):
+        if i not in seen:
+            seen[i] = True
+            new.append(i)
+        if (index+1)%k == 0:
+            print "".join(new)
+            new = []
+            seen.clear()
+
+print "Test 2"
 string = "AABCAAADA"
 k=3
 merge_the_tools(string, k)
